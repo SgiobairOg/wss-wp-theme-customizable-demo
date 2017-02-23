383aebc2d5008439a1a043a3591dc4b40832c72a
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2017-02-23 09:22:00 -0500

    Removed the sidebar widget.

--- functions.php
+++ functions.php
@@ -23,14 +23,6 @@ function cws_widgets_init() {
         'after_title'   => '</h2>',
     ) );
 
-    register_sidebar( array(
-        'name'          => 'Sidebar Widget Area',
-        'id'            => 'sidebar_widget_area',
-        'before_widget' => '<div class="widget ' . '%2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
 }
 
 add_action( 'widgets_init', 'cws_widgets_init' );
