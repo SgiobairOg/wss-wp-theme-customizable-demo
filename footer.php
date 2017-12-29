bd8467a5bf77b0aae874a77f685068bd40c58f75
Tarek Fouda <tareksfouda@gmail.com>
2017-12-29 10:32:17 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
-<?php do_action('cws_footer'); ?>
 
+
+
+</div>	<div id="footer">
+		<div id="footer_name">
+			Power Products, Inc.
+			<div id="footer_blocks_holder">
+				<div class="footer_info_block">PO Box 1163<br />Ashland, KY 41105-1163</div>
+				<div class="footer_info_block2">3225 Greenup Ave.<br />Ashland, KY 41105</div>
+				<div class="footer_info_block2">Phone:  606/329-2418<br />FAX:  606/324-7424</div>
+				<div class="cleardiv"></div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php wp_footer(); ?>
 <!-- CWS Platform -->
+</body>
+</html>
